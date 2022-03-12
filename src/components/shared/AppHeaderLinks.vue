f136d3f2debfe367361bf2b465977c18e49f5e2b
ArizonaTay <gratzxy@gmail.com>
2022-03-12 16:02:13 +0800

    Fix Nav bar
    Fix Nav bar

--- src/components/shared/AppHeaderLinks.vue
+++ src/components/shared/AppHeaderLinks.vue
@@ -3,12 +3,12 @@ export default {
 	props: ['isOpen'],
 };
 </script>
-
+h
 <template>
 	<!-- Header links -->
 	<div
 		:class="isOpen ? 'block' : 'hidden'"
-		class="m-0 sm:ml-4 mt-5 sm:mt-3 sm:flex p-5 sm:p-0 justify-center items-center shadow-lg sm:shadow-none"
+		class="sm:flex p-5 sm:p-0  shadow-lg sm:shadow-none"
 	>
 		<router-link
 			to="/Home"
@@ -41,16 +41,6 @@ export default {
 			>Bookmark</router-link
 		>
 
-		<div
-			class="border-t-2 pt-3 sm:pt-0 sm:border-t-0 border-primary-light dark:border-secondary-dark"
-		>
-			<button
-				class="font-general-medium sm:hidden block text-left text-md font-medium bg-indigo-500 hover:bg-indigo-600 text-white shadow-sm rounded-sm px-4 py-2 mt-2 duration-500"
-				aria-label="Login Button"
-			>
-				Login
-			</button>
-		</div>
 	</div>
 </template>
 
--- src/components/shared/AppHeaderLogin.vue
+++ src/components/shared/AppHeaderLogin.vue
@@ -47,17 +47,13 @@ export default {
 };
 </script>
 
+
+
 <template>
-	<nav id="nav" class="sm:container sm:mx-auto">
-		<!-- Header start -->
-		<div
-			class="z-10 max-w-screen-lg xl:max-w-screen-xl block sm:flex sm:justify-between sm:items-center my-6"
-		>
-			<!-- Header menu links and small screen hamburger menu -->
-			<div class="flex justify-between items-center px-4 sm:px-0">
-				<!-- Header logos -->
-				<div>
-					<router-link to="/"
+	
+	<nav class="container flex justify-around py-8 mx-auto bg-white">
+  <div class="flex items-center">
+    <div> <router-link to="/"
 						><img
 							src="@/assets/images/logo-light.png"
 							class="w-36"
@@ -65,58 +61,29 @@ export default {
 						/>
 					</router-link>
 				</div>
-
-				<!-- Small screen hamburger menu -->
-				<div class="sm:hidden">
-					<button
-						@click="isOpen = !isOpen"
-						type="button"
-						class="focus:outline-none"
-						aria-label="Hamburger Menu"
-					>
-						<svg
-							xmlns="http://www.w3.org/2000/svg"
-							viewBox="0 0 24 24"
-							class="h-7 w-7 fill-current text-secondary-dark dark:text-ternary-light"
-						>
-							<path
-								v-if="isOpen"
-								fill-rule="evenodd"
-								d="M18.278 16.864a1 1 0 0 1-1.414 1.414l-4.829-4.828-4.828 4.828a1 1 0 0 1-1.414-1.414l4.828-4.829-4.828-4.828a1 1 0 0 1 1.414-1.414l4.829 4.828 4.828-4.828a1 1 0 1 1 1.414 1.414l-4.828 4.829 4.828 4.828z"
-								clip-rule="evenodd"
-							></path>
-							<path
-								v-if="!isOpen"
-								fill-rule="evenodd"
-								d="M4 5h16a1 1 0 0 1 0 2H4a1 1 0 1 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2zm0 6h16a1 1 0 0 1 0 2H4a1 1 0 0 1 0-2z"
-							></path>
-						</svg>
-					</button>
-				</div>
-			</div>
-
-			<!-- Header links -->
-			<AppHeaderLinks :showModal="showModal" :isOpen="isOpen" />
-
-			<!-- Header right section buttons -->
-			<div
-				class="hidden sm:flex justify-between items-center flex-col md:flex-row"
-			>
-				<!-- Hire me button -->
-				<div class="hidden md:block">
-					<Button
+  </div>
+  <!-- left header section -->
+  <div class="items-center hidden space-x-8 lg:flex">
+	<AppHeaderLinks :showModal="showModal" :isOpen="isOpen" />
+  </div>
+  <!-- right header section -->
+  <div class="flex items-center space-x-2">
+   <Button
 						title="Logout"
-						class="text-md font-general-medium bg-indigo-500 hover:bg-indigo-600 text-white shadow-sm rounded-md px-5 py-2.5 duration-300"
+						class="text-md font-general-medium bg-indigo-500 hover:bg-indigo-600 text-white shadow-sm rounded-md px-3 py-2.5 duration-300"
 						@click="showModal()"
 						aria-label="Log out"
 					/>
-				</div>
-
-				
-			</div>
-		</div>
-	</nav>
+      
+    
+    
+  </div>
+</nav>
 </template>
+
+
+
+
 
 <style scoped>
 #nav a.router-link-exact-active {
