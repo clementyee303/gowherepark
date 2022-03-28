1dc5e1c763ebe201f8fb213f65dc64fb0fb71888
clementyee303 <yeeclement303@gmail.com>
2022-03-28 18:06:59 +0800

    undo prev

--- src/components/shared/AppHeaderLinksGuest.vue
+++ src/components/shared/AppHeaderLinksGuest.vue
@@ -34,7 +34,7 @@ h
 			aria-label="History"
 			>History</router-link
 		>
-		<router-linky
+		<router-link
 			to="/bookmark"
 			class="font-general-medium block text-left text-lg font-medium text-primary-dark dark:text-ternary-light hover:text-indigo-600 dark:hover:text-indigo-300  sm:mx-4 mb-2 sm:py-2 border-t-2 pt-3 sm:pt-2 sm:border-t-0 border-primary-light dark:border-secondary-dark"
 			aria-label="bookmark"
